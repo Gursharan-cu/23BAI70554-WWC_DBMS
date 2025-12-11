080be4712e344e94a41c1e79e4093224029abcc2
Gursharan-cu <23BAI70554@cuchd.in>
2025-12-12 00:16:43 +0530

    Add files via upload

---
+++ G5_SESSION-1/ques1_session1.sql
@@ -0,0 +1,20 @@
+-- G5_23BAI70147_Chandan_kumar_SESSION1
+CREATE TABLE employee (
+    emp_id SERIAL PRIMARY KEY,
+    emp_name VARCHAR(100),
+    department VARCHAR(50)
+);
+drop table employee
+INSERT INTO employee (emp_name, department) 
+VALUES 
+('Ajay', 'Engineering'),
+('Priya', 'Data Science'),
+('Rohit', 'Marketing')
+
+CREATE VIEW Employee_View AS
+SELECT emp_name, department 
+FROM employee
+
+CREATE ROLE Analyst
+GRANT SELECT ON Employee_View TO Analyst
+
